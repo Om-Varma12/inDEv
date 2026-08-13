abef44e3621042d40ae4fa6301a7fce6413352ff
Om Varma <omvarma1210@gmail.com>
2026-08-13 19:43:44 +0530

    add: groq api key rotation mech

--- server/src/services/llm/groq.service.ts
+++ server/src/services/llm/groq.service.ts
@@ -2,13 +2,33 @@ import Groq from "groq-sdk";
 import type { LLMMessage } from "../../types/llm.types.js";
 import "dotenv/config";
 
-const groq = new Groq({
-    apiKey: process.env.GROQ_API_KEY,
-});
+const keys = [
+    process.env.GROQ_API_KEY_1,
+    process.env.GROQ_API_KEY_2,
+    process.env.GROQ_API_KEY_3,
+    process.env.GROQ_API_KEY_4,
+    process.env.GROQ_API_KEY_5,
+    process.env.GROQ_API_KEY_6,
+].filter((key): key is string => Boolean(key));
+
+let currentIndex = 0;
+
+const getNextKey = () => {
+    const key = keys[currentIndex]
+    currentIndex = (currentIndex + 1) % keys.length
+
+    return key;
+}
 
 export const run = async (
     messages: LLMMessage[]
 ) => {
+    const apiKey = getNextKey();
+
+    const groq = new Groq({
+        apiKey
+    });
+
     const response = await groq.chat.completions.create({
         messages,
         model: "qwen/qwen3.6-27b",
